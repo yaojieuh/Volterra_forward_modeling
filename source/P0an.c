27ec616488ba198fc156ea09b5e2467f34c606b1
Jie Yao <njustyaojie@gmail.com>
2015-02-02 10:21:49 -0600

    initial project version'

---
+++ source/P0an.c
@@ -0,0 +1,341 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void greenfunction(double *r1, double *r2, double k,  double* G0)
+{
+	double r, coe;
+	double pi = 3.14159265358979323846; 
+	r=sqrt((r1[0]-r2[0])*(r1[0]-r2[0])+(r1[1]-r2[1])*(r1[1]-r2[1]));
+	coe=sqrt(1.0/(pi*k*r*8));
+	G0[0]=coe*sin(k*r-pi/4);
+	G0[1]=coe*cos(k*r-pi/4);
+	
+}
+void prodcomp( double *z1, double *z2, double *zres ){
+
+     // real part
+     zres[0] = z1[0]*z2[0]-z1[1]*z2[1]; 
+     // imaginary part  
+     zres[1] = z1[1]*z2[0]+z1[0]*z2[1]; 
+
+}
+
+
+void P0num(FILE *filefprintf, int nw, int nx, int nz,double dx,double dz, double *sourcefren, int *ps,  double *G0r,double *G0i, double *P0r,double *P0i){
+	int iw,ix,iz;
+        int xindex,zindex;
+   	double G0[2],sw[2],po[2];
+  	double x,z;
+ 	int nz2    = 2*nz+1; 
+   	int nx2    = 2*nx+1;
+
+	fprintf(filefprintf, "--!\tP0 calculation  ...\n");
+  	fprintf(filefprintf, "--!\t   		\n");
+
+	FILE* file2;  
+  	char fname2[100];
+  	
+        for(iw=0;iw<nw;iw++){
+		//omega=fren[iw];
+		sw[0]=sourcefren[2*iw];
+		sw[1]=sourcefren[2*iw+1];
+  		for(ix=0;ix<nx;ix++){
+			xindex=ix-ps[0]+nx;
+			for(iz=0;iz<nz;iz++){
+				zindex=iz-ps[1]+nz;
+				G0[0]=G0r[iw*(nx2*nz2)+xindex*nz2+zindex];
+				G0[1]=G0i[iw*(nx2*nz2)+xindex*nz2+zindex];
+				prodcomp(sw, G0, po );
+				P0r[iw*(nx*nz)+ix*nz+iz]=po[0];
+				P0i[iw*(nx*nz)+ix*nz+iz]=po[1];
+			}
+        		
+  		}
+		if((iw%50)==0){
+		sprintf(fname2,"P0num_iw_%d.dat", iw);
+		file2 = fopen(fname2,"w");
+ 		for (ix=0;ix<nx;ix++){
+			x=(ix-(nx-1)/2)*dx;
+			for (iz=0;iz<nz;iz++){
+				z=(iz-(nz-1)/2)*dz;		
+                          	fprintf(file2," %f %f %.12lf %.12lf \n", x, z, P0r[iw*(nx*nz)+ix*nz+iz],P0i[iw*(nx*nz)+ix*nz+iz]);
+			}
+                          fprintf(file2," \n");
+		}
+
+       		 fclose(file2);
+		}
+		
+	}
+}
+
+
+
+
+
+
+void greenfunction2(double x, double z, double k,  double* G00)
+{
+	double pi = 3.14159265358979323846; 
+	double Exp[2],exp2[2],exp3[2];
+	double kz2,kn;
+	int n,i;
+	G00[0]=0;
+	G00[1]=0;
+	double L=2000;
+	for (i=0;i<151;i++){
+		n=i-75;
+		kz2=k*k-(2*pi*n/L)*(2*pi*n/L);
+		Exp[0]=cos(2*pi*n*x/L);
+		Exp[1]=sin(2*pi*n*x/L);
+		if (kz2>0){
+			kn=sqrt(kz2);			
+			exp2[0]=cos(kn*fabs(z));
+			exp2[1]=sin(kn*fabs(z));
+			prodcomp(Exp, exp2, exp3 );
+			G00[0]+=exp3[1]/kn/2/L;
+			G00[1]+=exp3[0]/kn/2/L;
+		}
+		else{
+			kn=sqrt(-kz2);
+			exp2[0]=exp(-kn*fabs(z));
+			exp2[1]=0;
+			prodcomp(Exp, exp2, exp3 );
+			G00[0]-=exp3[0]/kn/2/L;
+			G00[1]-=exp3[1]/kn/2/L;
+		}
+		
+	}
+	
+}
+
+void greenfunction3(double x, double z, double k,  double* G0)
+{
+	double r, coe;
+	double pi = 3.14159265358979323846; 
+	r=sqrt(x*x+z*z);
+	if (r==0){
+		G0[0]=0;
+		G0[1]=-0.25;
+	}else{
+	coe=sqrt(1.0/(pi*k*r*8));
+	G0[0]=coe*sin(k*r-pi/4);
+	G0[1]=coe*cos(k*r-pi/4);
+	}
+	
+}
+
+void greenfunctionV(double x, double z, double k,  double* G00)
+{
+	double pi = 3.14159265358979323846; 
+	double Exp[2],exp2[2],exp3[2];
+	double kz2,kn;
+	int n,i;
+	G00[0]=0;
+	G00[1]=0;
+	double L=2000;
+	for (i=0;i<151;i++){
+		n=i-75;
+		kz2=k*k-(2*pi*n/L)*(2*pi*n/L);
+		Exp[0]=cos(2*pi*n*x/L);
+		Exp[1]=sin(2*pi*n*x/L);
+		if (kz2>0){
+			kn=sqrt(kz2);			
+			exp2[0]=cos(kn*fabs(z));
+			exp2[1]=-sin(kn*fabs(z));
+			prodcomp(Exp, exp2, exp3 );
+			G00[0]+=exp3[1]/kn/2/L;
+			G00[1]+=exp3[0]/kn/2/L;
+		}
+		else{
+			kn=sqrt(-kz2);
+			exp2[0]=exp(-kn*fabs(z));
+			exp2[1]=0;
+			prodcomp(Exp, exp2, exp3 );
+			G00[0]-=exp3[0]/kn/2/L;
+			G00[1]-=exp3[1]/kn/2/L;
+		}
+		
+	}
+	
+}
+void greenfunctiontable(FILE *filefprintf, int nx2, int nz2, double dx, double dz, int nw, double* fren, double c0,double* G0r,double* G0i,
+double* VG0r)
+{
+	int ix,iz,iw;
+	double omega, k;
+        double x,z;
+        double G00[2];
+
+	fprintf(filefprintf, "--!\tGreen's Function calculation  ...\n");
+  	fprintf(filefprintf, "--!\t                                   \n");
+	
+	FILE* file2;  
+  	char fname2[100];
+        for(iw=0;iw<nw;iw++){
+		fprintf(filefprintf, "--!\t  Frequency_%d     \n",iw);
+		omega=fren[iw];
+                k=omega/c0;
+	        for (ix=0;ix<nx2;ix++){
+			x=(ix-(nx2-1)/2)*dx;
+			for (iz=0;iz<nz2;iz++){
+				z=(iz-(nz2-1)/2)*dz;
+				greenfunction2(x, z, k, G00);
+				//greenfunction3(x, z, k, G00);
+				G0r[iw*(nx2*nz2)+ix*nz2+iz]=G00[0];
+				G0i[iw*(nx2*nz2)+ix*nz2+iz]=G00[1];
+				if(z>0){
+					greenfunctionV(x, z, k, G00);
+					VG0r[iw*(nx2*nz2)+ix*nz2+iz]=G0r[iw*(nx2*nz2)+ix*nz2+iz]-G00[0];
+					//VG0r[iw*(nx2*nz2)+ix*nz2+iz]=G0i[iw*(nx2*nz2)+ix*nz2+iz]-G00[1];
+					//VG0r[iw*(nx2*nz2)+ix*nz2+iz]=2*G00[0];
+				}else{
+					VG0r[iw*(nx2*nz2)+ix*nz2+iz]=0;
+				}
+			}
+		}
+
+		
+		if((iw%50)==0){
+		sprintf(fname2,"Green_iw_%d.dat", iw);
+		file2 = fopen(fname2,"w");
+ 		for (ix=0;ix<nx2;ix++){
+			x=(ix-(nx2-1)/2)*dx;
+			for (iz=0;iz<nz2;iz++){
+				z=(iz-(nz2-1)/2)*dz;		
+                          	fprintf(file2," %f %f %.12lf %.12lf %.12lf\n", x, z, G0r[iw*(nx2*nz2)+ix*nz2+iz],G0i[iw*(nx2*nz2)+ix*nz2+iz],VG0r[iw*(nx2*nz2)+ix*nz2+iz]);
+			}
+                          fprintf(file2," \n");
+		}
+
+       		 fclose(file2);
+		}
+	}	
+	
+}
+
+
+void P1num(FILE *filefprintf, int nw, int nx, int nz,double dx,double dz,double c0, int *ps, double *sourcefren,double* fren, double *VG0r, double* vpe, double *P0r,double *P0i, double *P1r,double *P1i){
+	int iw,ix,iz,ix2,iz2;
+        int xindex,zindex;
+   	double VG0,Prtemp,Pitemp;
+	double omega,k;
+  	double x,z;
+ 	int nz2    = 2*nz+1; 
+   	int nx2    = 2*nx+1;
+
+	fprintf(filefprintf, "--!\tP1 calculation  ...\n");
+  	fprintf(filefprintf, "--!\t   		\n");
+
+	FILE* file2;  
+  	char fname2[100];
+  	
+        for(iw=0;iw<nw;iw++){
+		omega=fren[iw];
+                k=omega/c0;
+		
+		for(iz=0;iz<nz;iz++){			
+  			for(ix=0;ix<nx;ix++){
+				P1r[iw*(nx*nz)+ix*nz+iz]=P0r[iw*(nx*nz)+ix*nz+iz];
+				P1i[iw*(nx*nz)+ix*nz+iz]=P0i[iw*(nx*nz)+ix*nz+iz];				
+				Prtemp=0;
+				Pitemp=0;
+				for(iz2=0;iz2<iz;iz2++){
+					zindex=iz-iz2+nz;
+					for(ix2=0;ix2<nx;ix2++){
+						xindex=ix-ix2+nx;
+						VG0=VG0r[iw*(nx2*nz2)+xindex*nz2+zindex];
+						Prtemp+=VG0*vpe[ix2*nz+iz2]*P1r[iw*(nx*nz)+ix2*nz+iz2];
+						Pitemp+=VG0*vpe[ix2*nz+iz2]*P1i[iw*(nx*nz)+ix2*nz+iz2];
+					}
+				}
+				Prtemp*=dx*dz*k*k;
+				Pitemp*=dx*dz*k*k;
+				P1r[iw*(nx*nz)+ix*nz+iz]+=Prtemp;
+				P1i[iw*(nx*nz)+ix*nz+iz]+=Pitemp;	
+						
+
+			}
+		}
+
+		if((iw%50)==0){
+		sprintf(fname2,"P1_iw_%d.dat", iw);
+		file2 = fopen(fname2,"w");
+ 		for (ix=0;ix<nx;ix++){
+			x=(ix-(nx-1)/2)*dx;
+			for (iz=0;iz<nz;iz++){
+				z=iz*dz;		
+                          	fprintf(file2," %f %f %.12lf %.12lf \n", x, z, P1r[iw*(nx*nz)+ix*nz+iz],P1i[iw*(nx*nz)+ix*nz+iz]);
+			}
+                          fprintf(file2," \n");
+		}
+
+       		 fclose(file2);
+		}
+	}
+}
+
+
+
+void Pwtot(int nw, int nx,  int nz,  double dw, double dx,double dz, double dt, int nt,  double* fren, double *P1r, double *P1i, double *Pret){
+	int ix,iw,it,iz;
+	double pi = 3.14159265358979323846; 
+	double t,x,z, arg=0;
+        for(iz=0;iz<nz;iz++){
+	for(ix=0;ix<nx;ix++){
+		x=(ix-(nx-1)/2)*dx;
+		for(it=0;it<nt;it++){
+			t=it*dt;
+			Pret[iz*nt*nx+ix*nt+it]=0;
+			for (iw=0;iw<nw;iw++){
+				arg = fren[iw]*t;		
+				Pret[iz*nt*nx+ix*nt+it]+=cos(arg)*P1r[iw*(nx*nz)+ix*nz+iz]-sin(arg)*P1i[iw*(nx*nz)+ix*nz+iz];
+			}
+			Pret[iz*nt*nx+ix*nt+it]*=dw*2/2/pi;
+			
+		}
+		
+		
+	}
+	}
+	FILE* file2;  
+  	char fname2[100];
+	for(it=0;it<nt;it++){
+		if((it%100)==0){
+		sprintf(fname2,"snapshot_%d.dat", it);
+		file2 = fopen(fname2,"w");
+ 		for (ix=0;ix<nx;ix++){
+			x=(ix-(nx-1)/2)*dx;
+			for (iz=0;iz<nz;iz++){
+				z=iz*dz;		
+                          	fprintf(file2," %f %f %.12lf \n", x, z, Pret[iz*nt*nx+ix*nt+it]);
+			}
+                          fprintf(file2," \n");
+		}
+
+       		 fclose(file2);
+		}
+	}
+
+	for(iz=0;iz<nz;iz++){
+		if((iz%10)==0){
+		sprintf(fname2,"trace_%d.dat", iz);
+		file2 = fopen(fname2,"w");
+ 		for (ix=0;ix<nx;ix++){
+			x=(ix-(nx-1)/2)*dx;
+			for (it=0;it<nt;it++){
+				t=it*dt;		
+                          	fprintf(file2," %f %f %.12lf \n", x, t, Pret[iz*nt*nx+ix*nt+it]);
+			}
+                          fprintf(file2," \n");
+		}
+
+       		 fclose(file2);
+		}
+	}
+	
+}
---
+++ source/Volterra_2D.c
@@ -0,0 +1,254 @@
+//!########################################################################
+//!                                                                       #
+//! Copyright (C) University .                     #
+//! This file is a modeling for 2d acoustic media  #
+//!                                                                       #
+//! 
+//! Feb. 2014                                                      # 
+//!                                                                       #
+//!########################################################################
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include "init.h"
+#include "P0an.h"
+//#include "Reflectiondata.h"
+
+int main( int argc, char *argv[] )
+{
+  // grids dimensions 
+  int nz    = 51; 
+  int nx    = 101;
+  int ndims = nx*nz;       // total grid points number
+
+
+
+  // physical grid size
+  double dz = 5.0;     // modeling grid spacial step in z
+  double dx = 5.0;    // modeling grid spacial step in x
+
+ 
+  FILE* file1;
+  char fname1[100];
+  int ret;
+  sprintf(fname1,"test_2D_acoustic.out");
+  file1 = fopen(fname1,"w");
+
+
+  fprintf(file1, "--!\t                                     \n");
+  fprintf(file1, "--!\t2D acoustic Volterra modeling research code\n");
+  fprintf(file1, "--!\t                                     \n");
+
+  ret = fflush(file1);
+
+  // velocity field
+  double c0=1500; //reference velocity
+  double* vel  = malloc( ndims*sizeof(double) );
+  double* vpe  = malloc( ndims*sizeof(double) );
+  //init_acou_homog( file1, dim_w, vel);
+  init_acou_layer(file1,  dx,dz,nx,nz, c0, vel,vpe);
+                                                                                                                                                                            
+
+ 
+   // time and frequency discretization parameter
+  int   nt = 1000;        // number of steps
+  double dt = 0.001;      // time step
+  int nw=101;
+  double dw=2;
+  double* sourcet = malloc(nt*sizeof(double) );
+  double* sourcefren  = malloc( (2*nw)*sizeof(double) );
+  double* fren  = malloc( nw*sizeof(double) );
+  init_source_ricker_fwps( file1, nt, dt,nw,dw, sourcet,fren, sourcefren); 
+
+
+  int* ps = malloc(2*sizeof(int));
+  double* pscor = malloc(2*sizeof(double));
+  ps[1] = 0;
+  ps[0] = (nx-1)/2;
+  pscor[1] = (ps[0])*dz;
+  pscor[0] = (ps[1])*dx;   
+
+   int nz2    = 2*nz+1; 
+   int nx2    = 2*nx+1;
+   double* G0r = malloc(nz2*nx2*nw*sizeof(double) );
+   double* G0i = malloc(nz2*nx2*nw*sizeof(double) );
+   double* VG0r = malloc(nz2*nx2*nw*sizeof(double) );
+   greenfunctiontable(file1,nx2, nz2, dx,dz, nw,  fren, c0,G0r, G0i,VG0r);
+
+
+   double* P0r = malloc(nz*nx*nw*sizeof(double) );
+   double* P0i = malloc(nz*nx*nw*sizeof(double) );
+   P0num(file1,  nw, nx, nz, dx,dz,sourcefren, ps,  G0r,G0i, P0r,P0i);
+
+   double* P1r = malloc(nz*nx*nw*sizeof(double) );
+   double* P1i = malloc(nz*nx*nw*sizeof(double) );
+   
+   P1num(file1,  nw, nx,  nz,dx, dz,c0, ps, sourcefren, fren, VG0r,  vpe, P0r,P0i, P1r,P1i);
+
+   nt=401;
+
+   double* Pret = malloc(nz*nx*nt*sizeof(double) );
+   
+   Pwtot(nw,nx, nz,  dw, dx, dz, dt, nt,fren,P1r, P1i, Pret);
+   // Pwtot(nw,nx,  nz,  dw, dx, dz, dt, nt,   fren,P0r, P0i, Pret);
+ 
+
+/*
+   double* precor = malloc(2*nx*sizeof(double));
+   int* prec = malloc(2*nx*sizeof(int));
+   
+
+        double pi = 3.14159265358979323846;
+   	int nk=nx,ik,iw,iz2;
+   	int nk2=(nk-1)/2;
+   	double dk=2*pi/(nx-1)/dx;
+	double r0,k,w,coef=0;
+	double ac,arg,k1,k2,q,z1,kr,ag;
+	double P0tempr, P0tempi;
+	double* P0Vr = malloc(nk*nz*sizeof(double));
+   	double* P0Vi = malloc(nk*nz*sizeof(double));
+	double* P0kr = malloc(nk*sizeof(double));
+   	double* P0ki = malloc(nk*sizeof(double));
+   	double* P0rewsr = malloc(nx*nz*sizeof(double));
+   	double* P0rewsi = malloc(nx*nz*sizeof(double));	
+        double* P0numr = malloc(nw*nx*sizeof(double));
+        double* P0numi = malloc(nw*nx*sizeof(double));
+	double* P0numsr = malloc(nw*nx*nz*sizeof(double));
+        double* P0numsi = malloc(nw*nx*nz*sizeof(double));
+	double* P0numlr = malloc(nw*nx*nz*sizeof(double));
+        double* P0numli = malloc(nw*nx*nz*sizeof(double));
+   	double* P0rewtr = malloc(nw*nx*nz*sizeof(double));
+  	double* P0rewti = malloc(nw*nx*nz*sizeof(double));
+  FILE* file2;  
+  char fname2[100];
+        sprintf(fname2,"green.dat");
+        file2 = fopen(fname2,"w");
+	
+	for (iz=1;iz<nz-300;iz++){		
+		z=iz*dz;
+		fprintf(file1,"Depth  %lf   \n",z);
+		for(ix=0;ix<nx;ix++){
+    			prec[2*ix] =  ps[0]+iz;
+    			prec[2*ix+1] =  ix;
+    			precor[2*ix] = (prec[2*ix])*dz;
+    			precor[2*ix+1] = (prec[2*ix+1])*dx;
+   
+   		}	
+		P0num(nx, nw,  c0,sourcefren, pscor, precor, P0numr, P0numi);
+		for(ix=0;ix<nx;ix++){
+			for(iw=0;iw<nw;iw++){
+				P0numsr[iw*nx*nz+iz*nx+ix]=P0numr[ix*nw+iw];
+				P0numsi[iw*nx*nz+iz*nx+ix]=P0numi[ix*nw+iw];
+			}	
+		}
+	}	
+
+   	for (iw=0;iw<nw;iw++){
+	        fprintf(file1,"Frequency  %lf   \n",iw*dw);
+		w=iw*dw+0.001;
+		k=w/c0;
+		
+		for (iz=0;iz<nz;iz++){	
+			for (ik=0;ik<nk;ik++){		
+				P0Vr[iz*nk+ik]=0;
+				P0Vi[iz*nk+ik]=0;
+			}
+			for (ix=0;ix<nx;ix++){		
+				P0rewsr[iz*nx+ix]=0;
+				P0rewsi[iz*nx+ix]=0;
+			}
+			
+		}		
+		
+		for (iz=1;iz<nz-300;iz++){
+			z=iz*dz;
+			for (ik=0;ik<nk;ik++){
+				kr=(ik-nk2)*dk;
+				P0Vr[(iz-1)*nk+ik]=0;
+				P0Vi[(iz-1)*nk+ik]=0;
+				for (ix=0;ix<nx;ix++){
+					ag=kr*precor[2*ix+1];
+					P0Vr[(iz-1)*nk+ik]+=(cos(ag)*P0rewsr[(iz-1)*nx+ix]+sin(ag)*P0rewsi[(iz-1)*nx+ix])*v[(iz-1)*nx+ix];
+					P0Vi[(iz-1)*nk+ik]+=(cos(ag)*P0rewsi[(iz-1)*nx+ix]-sin(ag)*P0rewsr[(iz-1)*nx+ix])*v[(iz-1)*nx+ix];
+				}
+				P0Vr[(iz-1)*nk+ik]*=dx/2/pi;
+				P0Vi[(iz-1)*nk+ik]*=dx/2/pi;
+							
+			}
+			for(ik=0;ik<nk;ik++){
+				kr=(ik-nk2)*dk;
+				k2=k*k-kr*kr;
+				P0kr[ik]=0;
+				P0ki[ik]=0;
+				if (k2>0.5*k*k){
+				//if (k2>=0){
+					q=sqrt(k2);
+					ag=q*z;		
+					P0tempr=0;
+					P0tempi=0;
+					for (iz2=0;iz2<iz;iz2++){
+						z1=iz2*dz;
+						k1=k*k/q;
+						P0tempr=sin(q*(z-z1))*P0Vr[iz2*nk+ik]*k1*dz;
+						P0tempi=sin(q*(z-z1))*P0Vi[iz2*nk+ik]*k1*dz;
+						P0kr[ik]+=P0tempr;
+						P0ki[ik]+=P0tempi;	
+					}
+						
+				}
+			
+					
+			}
+			
+			for (ix=0;ix<nx;ix++){
+				x=precor[2*ix+1];			
+				P0rewsr[iz*nx+ix]=0;
+				P0rewsi[iz*nx+ix]=0;
+				for(ik=0;ik<nk;ik++){
+					kr=(ik-nk2)*dk;
+					k2=k*k-kr*kr;						
+					if (k2>0){
+						ag=kr*x;
+						P0rewsr[iz*nx+ix]+=cos(ag)*P0kr[ik]-sin(ag)*P0ki[ik];
+						P0rewsi[iz*nx+ix]+=cos(ag)*P0ki[ik]+sin(ag)*P0kr[ik];
+					}
+				}
+				P0rewsr[iz*nx+ix]*=dk;
+				P0rewsi[iz*nx+ix]*=dk;
+				//P0rewsr[iz*nx+ix]=coef*(sourcefren[3*iw+1]*sin(k*r0-pi/4)+sourcefren[3*iw+2]*cos(k*r0-pi/4));
+				//P0rewsi[iz*nx+ix]=coef*(-sourcefren[3*iw+1]*cos(k*r0-pi/4)+sourcefren[3*iw+2]*sin(k*r0-pi/4));
+				P0rewsr[iz*nx+ix]+=P0numsr[iw*nx*nz+iz*nx+ix];
+				P0rewsi[iz*nx+ix]+=P0numsi[iw*nx*nz+iz*nx+ix];
+				P0rewtr[iw*nx*nz+iz*nx+ix]=	P0rewsr[iz*nx+ix];
+				P0rewti[iw*nx*nz+iz*nx+ix]=	P0rewsi[iz*nx+ix];
+				//fprintf(file2," %lf  %lf  %lf \n",  x,iz*dz,P0rewsr[iz*nx+ix]);
+							
+			}
+			
+			//fprintf(file2,"  \n");
+		}
+		
+	}
+    
+	double* Pswr1 = malloc(nw*nx*sizeof(double));
+        double* Pswi1 = malloc(nw*nx*sizeof(double));
+        double* Prets = malloc(nt*nx*sizeof(double));
+        for (ix=0;ix<nx;ix++){
+                for (iw=0;iw<nw;iw++){
+                        //Pswr1[ix*nw+iw]=P0rewtr[iw*nx*nz+90*nx+ix];
+                        //Pswi1[ix*nw+iw]=P0rewti[iw*nx*nz+90*nx+ix];
+			  Pswr1[ix*nw+iw]=P0numsr[iw*nx*nz+90*nx+ix];
+                          Pswi1[ix*nw+iw]=P0numsi[iw*nx*nz+90*nx+ix];
+			fprintf(file2," %lf  %lf  %lf \n",  ix*dx,iw*dw,Pswr1[ix*nw+iw]);
+                }
+		fprintf(file2,"  \n");
+        }
+	
+
+        //P0num(nx, nw,  c0,sourcefren, pscor, precor, Pswr1, Pswi1);
+        Pwtot(nx,  nw, dw, dt, nt, precor, Pswr1, Pswi1, Prets);
+*/
+}
